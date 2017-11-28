f64ecd447b045c1f8010685f18f8b5201020df64
Võ Lê Thanh Phú <volethanhphu174@gmail.com>
2017-11-28 10:16:34 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = new Object({
+  "TS" : "BS"
+})
+
+function updatePlaylist(playlist, name, title) {
+  playlist[name] = title
+  return playlist
+}
+
+function removeFromPlaylist(playlist, name) {
+  delete playlist.name
+  return playlist
+}
